9839d3c9c1e24742ba64536f69a8016141fd585c
Hazar Durmazoğlu <hexagon6449@gmail.com>
2021-02-25 00:11:10 +0300

    24.02.2021

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constants
         public static string MaintenanceTime = "Bakım Zamanı";
         public static string ProductsListed = "Ürünler listelendi.";
         public static string ProductDetailsListed="Ürün detayları listelendi.";
+        public static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün olabilir.";
+        public static string ProductNameAlreadyExist="Bu isimde zaten başka bir ürün var.";
+        public static string CategoryLimitExceeded="Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
     }
 }
